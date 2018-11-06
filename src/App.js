7dc8782a06dda5d5e0d694f0f3d18e80891828b3
Varit J Patel <varitjpatel@Varits-MacBook-Air.local>
2018-11-06 22:26:41 +0530

    Add starter code for tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component} from "react";
 import "./App.css";
+import Board from "./screens/Board.js";
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
     );
   }
 }
